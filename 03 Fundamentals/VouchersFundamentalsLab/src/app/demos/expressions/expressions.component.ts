5f6ad8305be16252f2dd1aacb167dd44eb134296
Administrator@SPDOM.LOCAL <Administrator@SPDOM.LOCAL>
2018-01-30 14:55:15 +0100

    initial commit first lab

---
+++ 03 Fundamentals/VouchersFundamentalsLab/src/app/demos/expressions/expressions.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expressions',
+  templateUrl: './expressions.component.html',
+  styleUrls: ['./expressions.component.css']
+})
+export class ExpressionsComponent implements OnInit {
+
+  title: string = "Expressions & Interpolation";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
