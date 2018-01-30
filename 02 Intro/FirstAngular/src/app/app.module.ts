0aecd8fe9ae03312d8906e3b3b5a3ee498f23588
Administrator@SPDOM.LOCAL <Administrator@SPDOM.LOCAL>
2018-01-30 10:45:01 +0100

    second chapter done

--- 02 Intro/FirstAngular/src/app/app.module.ts
+++ 02 Intro/FirstAngular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NestedComponent } from './nested/nested.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NestedComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
