91b2526a7da9bd7ab05ad491ea79af7d37826741
Administrator <Administrator@SP2016>
2018-02-01 15:59:22 +0100

    balanceaccount API finished and tested

--- 06 Forms & Validation/Voucherservice/VouchersAPI/VouchersAPI/Controller/Api/AccountsController.cs
+++ 06 Forms & Validation/Voucherservice/VouchersAPI/VouchersAPI/Controller/Api/AccountsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vouchers
 {
@@ -16,10 +17,49 @@ namespace Vouchers
       ctx = context;
     }
 
+    // http://localhost:PORT/api/accounts
     [HttpGet]
     public IEnumerable<BalanceAccount> Get()
     {
-        return ctx.BalanceAccounts;
+        var baccounts = ctx.BalanceAccounts.OrderByDescending(ba => ba.Created).ToList();
+        return baccounts;
     }
-  }
+
+    // http://localhost:PORT/api/accounts/3
+    [HttpGet("{id}")]
+    public BalanceAccount Get(int id)
+    {
+        return ctx.BalanceAccounts.FirstOrDefault(ba => ba.ID == id);
+    }
+
+    // http://localhost:PORT/api/accounts --> CREATE
+    [HttpPost]
+    public void Post([FromBody]BalanceAccount value)
+    {
+        ctx.BalanceAccounts.Add(value);
+        ctx.SaveChanges();
+    }
+
+    // http://localhost:PORT/api/accounts   --> UPDATE
+    [HttpPut()]
+    public void Put([FromBody]BalanceAccount value) //Classic .NET Core WebApi pattern: public void Put(int id, [FromBody]Voucher value)
+    {
+        ctx.BalanceAccounts.Attach(value);
+        ctx.Entry(value).State = EntityState.Modified;
+        ctx.SaveChanges();
+    }
+
+    // http://localhost:PORT/api/accounts   --> DELETE
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        var ba = Get(id);
+        if (ba != null)
+        {
+            ctx.Remove(ba);
+            ctx.SaveChanges();
+        }
+    }
+
+    }
 }
